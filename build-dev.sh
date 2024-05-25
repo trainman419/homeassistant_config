1fa0095ef276d99658fa5491375b6cd847cac40a
Austin Hendrix <legotown@aol.com>
2024-05-24 19:27:00 -0700

    Setup for dev instance, and update automations.

--- build-dev.sh
+++ build-dev.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-docker pull homeassistant/amd64-base-python
+#docker pull homeassistant/amd64-base-python
 
-cd home-assistant
+pushd home-assistant
 docker build -t home-assistant-dev . --build-arg BUILD_FROM=homeassistant/amd64-base-python
+popd
