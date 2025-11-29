646686c7ee9cb421d9a3771460ea4bf54467e91e
Austin Hendrix <legotown@aol.com>
2025-11-28 19:56:33 -0800

    Add HACS

--- start.sh
+++ start.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail
 
-IMAGE_NAME=homeassistant/home-assistant
-#IMAGE_NAME=home-assistant-dev
-
 # NOTE: may need `docker exec [container] pip install pyst2` to install pyst2
-docker start home-assistant || docker run -d --name="home-assistant" --restart unless-stopped -v /home/hendrix/home-assistant/config:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/serial/by-id/usb-0658_0200-if00:/zwaveusbstick:rw --net=host "$IMAGE_NAME"
+docker start home-assistant || ./create-homeassistant.sh
 #docker logs home-assistant --since 1m --follow 2>&1 | grep -i -B4 -A10 -e bluesound 
 docker logs home-assistant --since 1m --follow 2>&1 
