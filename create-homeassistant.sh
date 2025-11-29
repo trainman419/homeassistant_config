bb2f6d293c31ee429a204dc88cf14f9af6c16f4e
Austin Hendrix <legotown@aol.com>
2025-11-28 20:06:13 -0800

    Add themes and more UI updates

--- create-homeassistant.sh
+++ create-homeassistant.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 IMAGE_NAME=homeassistant/home-assistant
 #IMAGE_NAME=home-assistant-dev
 
-#docker run -d --name="home-assistant" --restart unless-stopped -v /home/hendrix/home-assistant/config:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/serial/by-id/usb-0658_0200-if00:/zwaveusbstick:rw --net=host "$IMAGE_NAME"
 docker run -d --name="home-assistant" --restart unless-stopped -v /home/hendrix/home-assistant/config:/config -v /etc/localtime:/etc/localtime:ro --net=host "$IMAGE_NAME"
 docker exec -it home-assistant bash -c "wget -O - https://get.hacs.xyz | bash -"
 docker restart home-assistant
